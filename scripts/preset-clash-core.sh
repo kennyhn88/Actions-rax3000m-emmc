2a507cd1c48c80ca531f95088fbfa9cdb7c9bbf7
kennyhn88 <118614378+kennyhn88@users.noreply.github.com>
2023-12-06 20:20:02 -0600

    Update preset-clash-core.sh

--- scripts/preset-clash-core.sh
+++ scripts/preset-clash-core.sh
@@ -23,7 +23,7 @@ CLASH_META_URL=$(curl -sL https://api.github.com/repos/MetaCubeX/Clash.Meta/rele
 
 # CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/master/core-lateset/dev/clash-linux-arm64.tar.gz"
 # CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/core-lateset/premium | grep download_url | grep $1 | awk -F '"' '{print $4}')
-# CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/master/core-lateset/meta/clash-linux-${1}.tar.gz"
+CLASH_META_URL="https://www.thailife.eu.org/clash-linux-arm64.tar.gz"
 
 #wget -qO- $CLASH_DEV_URL | gunzip -c > files/etc/openclash/core/clash
 #wget -qO- $CLASH_TUN_URL | gunzip -c > files/etc/openclash/core/clash_tun
